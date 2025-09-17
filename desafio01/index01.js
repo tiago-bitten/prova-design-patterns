a18149a0ef130e09ae385838810a46a331129f8e
Tiago Silveira De Bittencourt <tiago.40123@alunosatc.edu.br>
2025-09-17 19:29:33 -0300

    prova

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,40 @@
-console.log("Hello, World! 001");
+class Bicicleta {
+    move() {
+        return "Pedalando a bicicleta no pátio da UNISATC";
+    }
+}
+
+class Patinete {
+    move() {
+        return "Andando de patinete pelo estacionamento da UNISATC";
+    }
+}
+
+class Onibus {
+    move() {
+        return "Andando de ônibus e chegando na UNISATC";
+    }
+}
+
+class TransporteFactory {
+    static types = {
+        bicicleta: Bicicleta,
+        patinente: Patinete,
+        onibus: Onibus
+    };
+
+    static create(type) {
+        const TransporteClass = this.types[type];
+        if (!TransporteClass) {
+            throw new Error("Tipo de transporte ainda não suportado");
+        }
+
+        return new TransporteClass();
+    }
+}
+
+const tipo = "onibus";
+
+const transporte = TransporteFactory.create(tipo);
+
+console.log(transporte.move());
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,23 @@
-console.log("Hello, World! 002");
+class Config {
+    constructor() {
+        if (Config.instance) {
+            return Config.instance;
+        }
+        this.settings = {};
+        Config.instance = this;
+    }
+
+    setConfig(key, value) {
+        this.settings[key] = value;
+    }
+
+    getConfig(key) {
+        return this.settings[key];
+    }
+}
+
+const c1 = new Config();
+c1.setConfig("lang", "pt-BR");
+
+const c2 = new Config();
+console.log(c2.getConfig("lang"));
