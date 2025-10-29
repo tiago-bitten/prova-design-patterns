ae6cd53ce70f02d0fc8dd45d7a2b49c0f137f751
Tiago Silveira De Bittencourt <tiago.40123@alunosatc.edu.br>
2025-10-29 19:30:51 -0300

    teste

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,41 @@
-console.log("Hello, World! 001");
+class Message {
+    constructor(text) {
+        this.text = text;
+    }
+
+    getText() {
+        return this.text;
+    }
+}
+
+class MessageDecorator {
+    constructor(message) {
+        this.message = message;
+    }
+
+    getText() {
+        return this.message.getText();
+    }
+}
+
+class TitleMessageDecorator extends MessageDecorator {
+    getText() {
+        return "------\n" + super.getText() + "------";
+    }
+}
+
+class UppercaseMessageDecorator extends MessageDecorator {
+    getText() {
+        return super.getText().toUpperCase();
+    }
+}
+
+const msg = new Message("hoje o dia está horrível");
+
+const decorated = new HappyMessageDecorator(
+    new UppercaseMessageDecorator(
+        new CensorshipMessageDecorator(msg)
+    )
+);
+
+console.log(decorated.getText());
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,53 @@
-console.log("Hello, World! 002");
+class WorkItem {
+    add(item) {
+        throw new Error("Método 'add' não implementado");
+    }
+
+    showDetails() {
+        throw new Error("Método 'showDetails' não implementado");
+    }
+}
+
+class Task extends WorkItem {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+
+    showDetails(indent = 0) {
+        console.log(`${" ".repeat(indent)}- Tarefa: ${this.name}`);
+    }
+}
+
+class Project extends WorkItem {
+    constructor(name) {
+        super();
+        this.name = name;
+        this.items = [];
+    }
+
+    add(item) {
+        this.items.push(item);
+    }
+
+    showDetails(indent = 0) {
+        console.log(`${" ".repeat(indent)}Projeto: ${this.name}`);
+        this.items.forEach((item) => item.showDetails(indent + 2));
+    }
+}
+
+const t1 = new Task("Escrever documentação");
+const t2 = new Task("Fazer testes");
+const t3 = new Task("Publicar release");
+
+const subProject = new Project("Módulo de API");
+subProject.add(new Task("Criar endpoints"));
+subProject.add(new Task("Implementar autenticação"));
+
+const mainProject = new Project("Lançamento v1.0");
+mainProject.add(t1);
+mainProject.add(t2);
+mainProject.add(subProject);
+mainProject.add(t3);
+
+mainProject.showDetails();
